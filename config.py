91cc18c022a43df4ef92a9bc9433fee481062399
Tony Samaniego <tms.ocmp@gmail.com>
2022-10-16 11:07:24 -0700

    TD-52 Added additional news config variables

--- config.py
+++ config.py
@@ -9,4 +9,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # A newscatcher api key is required for headline news. https://newscatcherapi.com/
-    NEWSCATCHER_API_KEY = os.environ.get('NEWSCATCHER_API_KEY')
+    NEWSCATCHER_API_KEY = os.environ.get('NEWSCATCHER_API_KEY') or None
+    NEWSCATCHER_API_URL = "https://api.newscatcherapi.com/v2/search"
+    HEADLINES_ARTICLE_CNT = 20
