9fc870002fd4192385a3409c8ee561d520716cb1
Tony Samaniego <tms.ocmp@gmail.com>
2022-10-16 11:13:01 -0700

    TD-52 search_news_results() added to News class

--- app/media/routes.py
+++ app/media/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, redirect
 from flask_login import current_user, login_required
 from app import db
 from app.media import bp
+from app.models import News
 from datetime import datetime as dt
 
 # development data
@@ -10,14 +11,19 @@ from app.models._development_data import dev_articles
 @bp.route('/headlines')
 @login_required
 def headlines():
+    stock_names = current_user.portfolio_corporate_names
+    status_code, articles, message = News.search_news_results(stock_names, 1)
+    print(status_code)
+    print(message)
     # This code segment is temporary and will be replace in TD-52.
     # In TD-52 I'll add the proper validations and condition handeling.
     # I'm adding it for the sprint retrospective for when we demo.
     # ===========================================================
-    articles = dev_articles
-    for article in articles:
-        date_str = article['published_date']
-        article['published_date'] = dt.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+    # articles = dev_articles
+    # if isinstance(articles[0]['published_date'], str):
+    #     for i in range(len(articles)):
+    #         date_str = articles[i]['published_date']
+    #         articles[i]['published_date'] = dt.strptime(date_str, '%Y-%m-%d %H:%M:%S')
     # ===========================================================
     return render_template('media/headlines.html', 
                            title='News Headlines', articles=articles)
--- app/models/news.py
+++ app/models/news.py
@@ -1,4 +1,7 @@
+from requests import request
+from datetime import datetime as dt
 from app import db
+from flask import current_app
 
 class News(db.Model):
     __tablename__ = 'news'
@@ -19,4 +22,31 @@ class News(db.Model):
     excerpt = db.Column(db.String(EXCERPT_LENGTH))
     media = db.Column(db.String(URL_LENGTH))
 
+    def search_news_results(query, page) -> list:
+        formatted_query = ' OR '.join(['"' + name + '"' for name in query])
+        params = {
+            'q': formatted_query,
+            'countries': 'US',
+            'lang': 'en',
+            'sort_by': 'date',
+            'page_size': current_app.config['HEADLINES_ARTICLE_CNT'],
+            'to_rank': 10000,
+            'page': page
+        }
+        headers = {'x-api-key': current_app.config['NEWSCATCHER_API_KEY']}
+        response = request('GET', current_app.config['NEWSCATCHER_API_URL'],
+                            headers=headers, params=params)
+        status_code = response.status_code
+
+        if status_code == 200:
+            articles = response.json()['articles']
+            for i in range(len(articles)):
+                articles[i]['published_date'] = dt.strptime(
+                    articles[i]['published_date'], '%Y-%m-%d %H:%M:%S')
+            message = 'Success'
+        else:
+            articles = []
+            message = response.json()['message']
+
+        return [status_code, articles, message]
     
