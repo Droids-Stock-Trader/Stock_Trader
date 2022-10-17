ec164358cd03391d9857bdb3b0d844fff6f7e259
Tony Samaniego <tms.ocmp@gmail.com>
2022-10-17 08:35:31 -0700

    TD-52 Implemented news page back end

--- app/media/routes.py
+++ app/media/routes.py
@@ -1,29 +1,31 @@
-from flask import render_template, url_for, redirect
+from flask import current_app, render_template, abort, flash
 from flask_login import current_user, login_required
 from app import db
 from app.media import bp
 from app.models import News
-from datetime import datetime as dt
 
-# development data
-from app.models._development_data import dev_articles
 
-@bp.route('/headlines')
+@bp.route("/headlines")
 @login_required
 def headlines():
+    # gets the corporate names of all the stocks within
+    # the current users portfolio in list form
     stock_names = current_user.portfolio_corporate_names
-    status_code, articles, message = News.search_news_results(stock_names, 1)
-    print(status_code)
-    print(message)
-    # This code segment is temporary and will be replace in TD-52.
-    # In TD-52 I'll add the proper validations and condition handeling.
-    # I'm adding it for the sprint retrospective for when we demo.
-    # ===========================================================
-    # articles = dev_articles
-    # if isinstance(articles[0]['published_date'], str):
-    #     for i in range(len(articles)):
-    #         date_str = articles[i]['published_date']
-    #         articles[i]['published_date'] = dt.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-    # ===========================================================
-    return render_template('media/headlines.html', 
-                           title='News Headlines', articles=articles)
+    # calls the news api based on the given corporate names
+    if len(stock_names) > 0:
+        status_code, articles, message = News.search_news_results(stock_names, 1)
+    # if the user has an empty portfolio, the user is presented
+    # with the latest finantial, business, and economic news.
+    else:
+        status_code, articles, message = News.latest_headlines(1)
+    # if there was an error, redirect to 500 internal error page.
+    if status_code != 200:
+        flash(message)
+        abort(500)
+    return render_template(
+        "media/headlines.html",
+        title="News Headlines",
+        articles=articles,
+        status_code=status_code,
+        message=message,
+    )
--- config.py
+++ config.py
@@ -11,4 +11,5 @@ class Config(object):
     # A newscatcher api key is required for headline news. https://newscatcherapi.com/
     NEWSCATCHER_API_KEY = os.environ.get('NEWSCATCHER_API_KEY') or None
     NEWSCATCHER_API_URL = "https://api.newscatcherapi.com/v2/search"
-    HEADLINES_ARTICLE_CNT = 20
+    NEWSCATCHER_API_GEN_URL = 'https://api.newscatcherapi.com/v2/latest_headlines'
+    HEADLINES_ARTICLE_CNT = 25
