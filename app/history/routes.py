67d3e88f06df99bc5d067168cd67ccf63752e31b
Tony Samaniego <tms.ocmp@gmail.com>
2022-10-31 12:37:51 -0700

    TD-67 Initial History page BE

--- app/history/routes.py
+++ app/history/routes.py
@@ -3,8 +3,6 @@ from flask_login import current_user, login_required
 from app import db
 from app.history import bp
 
-from app.models import dev_account_history
-
 
 @bp.route('/account_history')
 @login_required
@@ -12,5 +10,7 @@ def account_history():
     """
     Renders account history page.
     """
-    data = dev_account_history
-    return render_template('history/account_history.html', title='Account History', data=data)
+    history = current_user.history_list
+    return render_template(
+        'history/account_history.html', 
+        title='Account History', history=history)
