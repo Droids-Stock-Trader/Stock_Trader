c3a2b685e3366f3984564d37445bafc473a1d39a
Tony Samaniego <tms.ocmp@gmail.com>
2022-10-31 12:40:37 -0700

    TD-68 Account history page pagination

--- app/history/routes.py
+++ app/history/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, current_app, request
 from flask_login import current_user, login_required
 from app import db
 from app.history import bp
+from app.models import History
 
 
 @bp.route('/account_history')
@@ -10,7 +11,23 @@ def account_history():
     """
     Renders account history page.
     """
-    history = current_user.history_list
+    # gets the next page for pagination
+    page = request.args.get('page', 1, type=int)
+    # queries the current page of history records
+    # records are sorted by date in decending order
+    history = History.query.filter_by(
+        user_id=current_user.id).order_by(
+            History.timedata.desc()).paginate(
+                page=page, 
+                per_page=current_app.config['HISTORY_LISTING_CNT'],
+                error_out=False)
+    # previous/next url for the pagination links
+    prev_url = url_for('history.account_history', page=history.prev_num) \
+        if history.has_prev else None
+    next_url = url_for('history.account_history', page=history.next_num) \
+        if history.has_next else None
+
     return render_template(
         'history/account_history.html', 
-        title='Account History', history=history)
+        title='Account History', history=history.items,
+        prev_url=prev_url, next_url=next_url)
--- config.py
+++ config.py
@@ -8,13 +8,14 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     # A newscatcher api key is required for headline news. https://newscatcherapi.com/
     NEWSCATCHER_API_KEY = os.environ.get('NEWSCATCHER_API_KEY') or None
     NEWSCATCHER_API_URL = "https://api.newscatcherapi.com/v2/search"
     NEWSCATCHER_API_GEN_URL = 'https://api.newscatcherapi.com/v2/latest_headlines'
     HEADLINES_ARTICLE_CNT = 25
 
-     # Email Configuration Variables
+    # Email Configuration Variables
     MAIL_SERVER = 'smtp.sendgrid.net'
     MAIL_PORT = 587
     MAIL_USE_TLS = 1
@@ -22,3 +23,6 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('SENDGRID_API_KEY')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
     ADMINS = [MAIL_DEFAULT_SENDER]
+
+    # History configurtion variables
+    HISTORY_LISTING_CNT = 2
