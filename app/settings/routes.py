c87b4f0eeceab85155633a105b1d13a2c89aadf6
Tony Samaniego <tms.ocmp@gmail.com>
2022-11-06 15:18:59 -0800

    TD-65 Notification settings account history

--- app/settings/routes.py
+++ app/settings/routes.py
@@ -30,17 +30,16 @@ def user_preferences():
             current_user.email = form.email.data
             current_user.dob = form.dob.data
             current_user.contact_pref = int(form.contact_preference.data)
-            # generates the account history record 
+            # generates the account history record
             # and saves it to the user profile
             record = History(
-                title="User Preferences Updated",
+                title="User Profile Updated",
                 description='The following changes were made to the ' +
-                            'user profile: ' + 
-                            f'{_format_changes(changes_made)}.'
+                    f'user profile: {_format_changes(changes_made)}.'
             )
             current_user.store_history_record(record)
             db.session.commit()
-            flash("User preferences have been saved")
+        flash("User profile have been saved")
     # populates the profile page with the current users attributes
     form.username.data = current_user.username
     form.phone_number.data = current_user.phone_num
@@ -48,7 +47,8 @@ def user_preferences():
     form.dob.data = current_user.dob
     form.contact_preference.data = str(current_user.contact_pref)
 
-    return render_template('settings/preferences.html', title='User Profile', form=form)
+    return render_template(
+        'settings/preferences.html', title='User Profile', form=form)
 
 
 @bp.route('/notifications', methods=['GET', 'POST'])
@@ -65,44 +65,75 @@ def user_notifications():
     form = NotificationForm()
 
     if form.validate_on_submit():
-        current_user.account_change_notify = form.account_change.data
-        current_user.holds_notify = form.holds.data
-        current_user.watchlist_notify = form.watchlist.data
-        db.session.commit()
+        # generates a list of notification settings that have been changed.
+        changes_made = _notification_changes(form)
+        if len(changes_made) > 0:
+            # Updates the user attributes
+            current_user.account_change_notify = form.account_change.data
+            current_user.holds_notify = form.holds.data
+            current_user.watchlist_notify = form.watchlist.data
+            # Creates a record of what was changed and saves the record 
+            # to the users account history.
+            record = History(
+                title='Notification Settings Updated',
+                description='The following notification settings ' + 
+                    f'changed: {_format_changes(changes_made)}.'
+            )
+            current_user.store_history_record(record)
+            db.session.commit()
         flash('Notification setting have been saved.')
-
+    # populates the notification page with the current user notification settings
     form.account_change.data = current_user.account_change_notify
     form.holds.data = current_user.holds_notify
     form.watchlist.data = current_user.watchlist_notify
 
-    return render_template('settings/notifications.html', title='Notification Settings', form=form)
+    return render_template(
+        'settings/notifications.html', 
+        title='Notification Settings', form=form)
 
 
 @bp.route('/headlines', methods=['GET', 'POST'])
 @login_required
 def news_settings():
     form = HeadlinesForm()
-    return render_template('settings/news_settings.html', title='News Settings', form=form)
+    return render_template(
+        'settings/news_settings.html', title='News Settings', form=form)
 
 
-def _profile_changed(form) -> bool:
+def _profile_changed(form) -> list:
     """
     Generates a list of user attributes that
     have been changed within the settings profile
     page.
     """
-    changes_made = []
+    changes = []
     if current_user.username != form.username.data:
-        changes_made.append("Username")
+        changes.append("Username")
     if current_user.phone_num != form.phone_number.data:
-        changes_made.append("Phone Number")
+        changes.append("Phone Number")
     if current_user.email != form.email.data:
-        changes_made.append("Email")
+        changes.append("Email")
     if current_user.dob.date() != form.dob.data:
-        changes_made.append("Date of Birth")
+        changes.append("Date of Birth")
     if current_user.contact_pref != int(form.contact_preference.data):
-        changes_made.append("Contact Preference")
-    return changes_made
+        changes.append("Contact Preference")
+    return changes
+
+
+def _notification_changes(form) -> list:
+    """
+    Generates a list of user notification settings
+    that have been changed within the notifications
+    settings page.
+    """
+    changes = []
+    if current_user.account_change_notify != form.account_change.data:
+        changes.append("Account Profile")
+    if current_user.holds_notify != form.holds.data:
+        changes.append("Holdings")
+    if current_user.watchlist_notify != form.watchlist.data:
+        changes.append("Account Portfolio")
+    return changes
 
 
 def _format_changes(changes) -> str:
